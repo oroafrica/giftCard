00fd5d04a7e20697b3e596d9a76d3e0bf2043d91
oroafrica <moegamat@oroafrica.com>
2020-10-29 13:16:25 +0200

    draft-01

--- js/card.js
+++ js/card.js
@@ -8,14 +8,13 @@ class Gift
 		this.fontCount = 0;
 		this.cards =(a)=>{return `https://oroafrica.github.io/giftCard/images/card_${a}.png`};
 		this.myFont = (a)=>{return ["BrushSignature","SoftSignature","Hero","Lovely","Poppins","Bronwilla"][a]};
-		this.themeNo = 1;
+		this.themeNo = 2;
 		this.fontColCount = 0;
-		this.fontColours = (a)=>{return ["#000","#838383","#5d5d5d"][a]};
-
+		this.fontColours = (a)=>{return ["#ff6961","#82d1f1","#838383","#5d5d5d","#000"][a]};
+		this.textAlign = (a)=>{return ["left","center","right"][a]};
+		this.alignCount = 0;
 		
-		
-
-		
+		this.config();
 
 		return this.render.bind(this)();
 	}
@@ -47,10 +46,13 @@ class Gift
 					,left:this.canvas.width/2
 					,objectCaching:false
 					,textAlign:"center"
-					,top:100
+					,top:100,
+					fontSize:40,padding:20
 				};
 				var s = new fabric.IText("Text",props);
+				
 				this.canvas.add(s);
+
 				s.animate("top",this.canvas.height/2,
 				{
 					start:0
@@ -59,9 +61,9 @@ class Gift
 				});
 				
 				this.canvas.setActiveObject(s);
-				// s.enterEditing();
+				s.enterEditing();
 				this.noteCount +=1;
-				
+			
 			}
 		});
 	}
@@ -129,64 +131,97 @@ class Gift
 	debug(isDebug)
 	{
 		$(document).on("click",".btn",(e)=>{
-		let report = {init:"Starting Gift Card!",fonts:this.myFont(this.fontCount),cards:this.cards(this.themeNo),textObject:this.noteCount,fontColour:this.fontColCount-1};
+		let report = {init:"Starting Gift Card!",fonts:this.myFont(this.fontCount),cards:this.cards(this.themeNo),textObject:this.noteCount,fontColour:this.fontColCount-1,alignIdex:this.alignCount-1};
 		this.msg(JSON.stringify(report));
 		});
 	}
-	deleteObject()
-	{
-		fabric.Object.prototype.cornerStyle = 'round';
-		fabric.Object.prototype.cornerColor = 'blue';
-		fabric.Object.prototype.transparentCorners = true;
-		fabric.Object.prototype.cornerSize=25;
 
+	config()
+	{
+		//basic settings
+		
+		fabric.Object.prototype.customiseCornerIcons({
+			settings: {
+				borderColor: '#000',
+				cornerSize: 25,
+				cornerShape: 'circle',
+				cornerBackgroundColor: 'teal',
+				cornerPadding:7,
+			}
+			,tl:{icon: 'icons/ico/delete.svg',cornerColor:"red"}
+			,tr:{icon: 'icons/ico/scale.svg'}
+			,ml:{}
+			,mr:{}
+			,mt:{}
+			,mb:{icon: 'icons/ico/align.svg'}
+			,bl:{icon: "icons/ico/palette.svg"}
+			,br:{icon:"icons/ico/font.svg"}
+			,mtr:{icon: 'icons/rotate.svg'}
+		});
 
 		fabric.Canvas.prototype.customiseControls({
-		tl: {
-			action: 'rotate',
-			cursor: 'hover',
-			icon:"../icons/remove.svg"
-		},
-		tr: {
-			action: 'scale',
-			icon:'remove',
-			cursor:'remove'
-		},
-		bl: {
-			action: 'remove',
-			cursor: "../icons/up.svg",
-		},
-		br: {
-			action: 'moveUp',
-			cursor: 'pointer',
-		},
-		mb: {
-			action: 'moveDown',
-			cursor: 'pointer',
-		},
-		mr: {
-			action: function(e, target) {
-				target.set({
-					left: 200,
-				});
-				this.canvas.renderAll();
+			tl: {
+				cursor:"pointer"
+				,action: 'remove'
 			},
-			cursor: 'pointer',
-		},
-		mt: {
-			action: {
-				'rotateByDegrees': 30,
+			tr: {
+				action: 'scale'
 			},
-			cursor: 'pointer',
+			bl: {
+				cursor: 'pointer'
+				,action: ()=>{
+						let s = this.canvas.getActiveObject();
+						this.fontColCount = (this.fontColCount > 4) ? 0 :this.fontColCount;
+						s.set({fill:this.fontColours(this.fontColCount)});
+						this.fontColCount +=1;
+						this.canvas.discardActiveObject().renderAll();
+						this.canvas.setActiveObject(s);}
+			},
+			br: {
+				cursor: 'pointer'
+				,action: ()=>{
+					this.fontCount = (this.fontCount > 5) ? 0 : this.fontCount;
+					let obj = this.canvas.getActiveObject();
+					obj.set({fontFamily:this.myFont(this.fontCount)});
+					this.canvas.renderAll();
+					this.fontCount++;
+				}
+				
+			},
+			mb: {
+				cursor: 'pointer'
+				,action: ()=>{
+					let s = this.canvas.getActiveObject();
+					this.alignCount = (this.alignCount > 2) ? 0 : this.alignCount;
+					s.set({textAlign:this.textAlign(this.alignCount)});
+					this.alignCount++;
+					this.canvas.discardActiveObject().renderAll();
+				    this.canvas.setActiveObject(s);
+				}
+			},
+			mr: {
+				action: function(e, target) {
+					target.set({
+						left: 200,
+					});
+					this.canvas.renderAll();
+				},
+				cursor: 'pointer',
+			},
+			mt: {
+				action: 'moveUp',
+				cursor: 'pointer',
+			},
+			// only if hasRotatingPoint is not set to false
+			mtr: {
+				action: 'rotate',
+				cursor: 'default',
+			}
 		}
-	});
-		  
-
+		,()=>{this.canvas.renderAll();});
 	}
 	render()
 	{
-		
-	
 		this.init();
 		this.addText();
 		this.clearText();
@@ -195,7 +230,7 @@ class Gift
 		this.serializeCanvas();
 		this.changeColour();
 		this.debug();
-		this.deleteObject();
+		
 	}
 	
 }
